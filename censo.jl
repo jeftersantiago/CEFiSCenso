656f63243ae226c3d6f702165099b7342f5f451f
jefter66 <jeftersantiago@protonmail.com>
2021-08-26 15:21:08 -0300

    funcao que faz levantamento das respostas por optativa funcionando

--- censo.jl
+++ censo.jl
@@ -9,44 +9,25 @@ option = CSV.read("opt_inst.csv", DataFrame)
 select!(data, Not(1:3))
 select!(option, Not(1:3))
 
+# lista com as perguntas
+question = getindex(names(option), 1:length(names(option)))
+# como declarar lista com opções
+# opt = option[!,question[i]]
+
 # sobre número de pessoas que responderam
 #x = ["Física", "Ciências Físicas e Biomoleculares", "Física Computacional"]
 # número de pessoas em cada curos
-# TODO: deixa dinamico
 #y = [33, 33, 33]
 # gráfico em barra para o número de pessoas em cada curso
 #pie(x, y, title = "Quantidade de alunos que responderam por curso", l = 0.5)
 
 
-question = getindex(names(option), 1:length(names(option)))
-
-# Exemplo de como recuperar lista de valores
-# ano = data[!, perguntas[1]]
-#ponderada(df[!, perguntas[3]])
-
-# create a funtion that runs the command df[!,quest[x]]
-
-# thrghout the whole dataset and it must do the following:
-
-# 1) Create a dynamic array of values that are going to increment
-# every time an equal option appears.
-
-# 2) Store, only one time, the alternative, a string.
-
-# 3) Remember to check in the loop if the alternative in the loop
-# was already stored.
-# at the end of the function call the barGraph()
-
-# 4) Store images in the barGraph() function.
-
 # Percorre a lista de alternativa e remove os indices missing 
 # Retorna a lista de opções, a partir da lista é possivel chamar a função
 # de gráfico e etc.
-# opt = option[!,question[i]]
 function alternatives(opt) 
     i = 1;
     n = length(opt) -  length(findall(ismissing,opt));
-    # não sei declarar array direito então aqui vai a gambiarra
     # declarei dessa forma e no final do loop removo o primeiro valor usando
     # a função popfirst
     list = [""] 
@@ -61,29 +42,35 @@ function alternatives(opt)
     return reverse(list,1,length(list));
 end
 
-
-
-# isso vai ser alterado
-# Gráfico das médias ponderadas
-function ponderada(list)
-    # não gosto disso, mas não pensei em nenhuma fórmula melhor ainda
-    option=["Entre 0 e 4", "Entre 4.1 e 6", "Entre 6.1 e 8", "Entre 8.1 e 9", "Entre 9.1 e 10"]
-    vals = [0, 0, 0, 0, 0]; 
-    i=1
-    while i in 1:length(list)
-        if getindex(list,i)==option[1] vals[1]+=1 end
-        if getindex(list,i)==option[2] vals[2]+=1 end
-        if getindex(list,i)==option[3] vals[3]+=1 end
-        if getindex(list,i)==option[4] vals[4]+=1 end
-        if getindex(list,i)==option[4] vals[5]+=1 end
-        i+=1
+# 1) Create a dynamic array of values that are going to increment
+# every time an equal option appears.
+# run through the array and list in a new array the values 
+# optList must come from alternatives(x)
+function setData(dataList,optList)
+# list is the list with the answers accounted for each option
+    # declarer array that will be counting
+    list = Array{Int64,1}(undef,length(optList)); 
+    i=1;
+    while i in 1:length(optList)
+        list[i] = 0;
+        x=1;
+        while x in 1:length(dataList)
+            if isequal(string(optList[i]), string(dataList[x]))
+                list[i]+=1;
+            end
+            x+=1;
+        end
+        i+=1;
     end
-    barGraph("Média ponderada (com reprovações)",[1,2,3,4,5], option , vals)
+    return list;
 end
 
+# it's working
+opt = alternatives(option[!,question[3]]);
+ans = data[!,question[3]];
+out = setData(ans,opt)
 
-# Exemplo de uso 
-#option = ["Muito Insatisfeito", "Insatisfeito", "Pouco Insatisfeito", "Satisfeito", "Muito Satisfeito"]
-#ans = [34,44,54,43,13];
-# barGraph("Pergunta",[1,2,3,4,5], option, ans)
-#                    [1:5] não funcionou :o 
+
+
+
+
--- graph.jl
+++ graph.jl
@@ -1,6 +1,7 @@
 using Printf, Plots
 using StatsPlots
 # Gera um gráfico em barra com as opções
+# TODO Store images in the barGraph() function.
 function barGraph(questTitle, x , options, ans )
 y = ans; 
 Plots.bar(x,y, title=questTitle,xticks=(1:length(x), options), legend=false,grid=false)
