fb35f5a9c1199c5fee757910e5cd6095ffe90917
jefter66 <jeftersantiago@protonmail.com>
2021-08-26 21:01:13 -0300

    pronto... por agora

--- censo.jl
+++ censo.jl
@@ -1,7 +1,6 @@
 using CSV, DataFrames
 include("graph.jl")
 
-
 # deixar isso dinamico também, permitir passar o nome do arquivo em uma função
 data = CSV.read("inst.csv", DataFrame)
 option = CSV.read("opt_inst.csv", DataFrame)
@@ -24,16 +23,16 @@ question = getindex(names(option), 1:length(names(option)))
 #pie(x, y, title = "Quantidade de alunos que responderam por curso", l = 0.5)
 
 
-# Percorre a lista de alternativa e remove os indices missing 
+# Percorre a lista de alternativa e remove os indices missing
 # Retorna a lista de opções, a partir da lista é possivel chamar a função
 # de gráfico e etc.
-function alternatives(opt) 
+function alternatives(opt)
     i = 1;
     n = length(opt) -  length(findall(ismissing,opt));
     # declarei dessa forma e no final do loop removo o primeiro valor usando
     # a função popfirst
-    list = [""] 
-    while i in 1:length(opt) 
+    list = [""]
+    while i in 1:length(opt)
         if !isequal(opt[i],missing)
             # usa o push aqui pq não quero ter que lidar com o indice em cada array
             push!(list, string(opt[i]));
@@ -46,21 +45,19 @@ end
 
 # 1) Create a dynamic array of values that are going to increment
 # every time an equal option appears.
-# run through the array and list in a new array the values 
+# run through the array and list in a new array the values
 # optList must come from alternatives(x)
 function setData(dataList,optList)
 # list is the list with the answers accounted for each option
     # declarer array that will be counting
-    list = Array{Int64,1}(undef,length(optList)); 
+    list = Array{Int64,1}(undef,length(optList));
     i=1;
     while i in 1:length(optList)
         list[i] = 0;
         x=1;
-#        println("1º Loop: " ,  i);
         while x in 1:length(dataList)
             if isequal(string(optList[i]), string(dataList[x]))
                 list[i]+=1;
-#                println("---- 2º Loop: " , i);
             end
             x+=1;
         end
@@ -70,12 +67,23 @@ function setData(dataList,optList)
 end
 
 
-function testar(n)
+function run(n)
     println(question[n]);
     opt = alternatives(option[!,question[n]]);
     println(opt);
     ans = data[!,n];
     out = setData(ans,opt);
-    println("Number of answers = " , out[1] + out[2] + out[3] + out[4] + out[5])
-    barGraph(question[n], [1:length(opt)], opt, out);
+    #println("Number of answers = " , out[1] + out[2] + out[3] + out[4] + out[5])
+    fileName = string("pergunta",string(n));
+    barGraph(question[n], [1:length(out)], opt, out, fileName );
 end
+
+j=1;
+while j in 1:43
+ println("Arquivo nº " , j);
+ run(j);
+ global j+=1;
+end
+
+
+
--- graph.jl
+++ graph.jl
@@ -1,18 +1,16 @@
 using Printf, Plots
 using StatsBase
-using Statistics
 using StatsPlots
-using LaTeXStrings
-# TODO Store images in the barGraph() function.
-function barGraph(question, x , options, ans )
-#Plots.bar(x,y, title=question,xticks=([1,2,3,4,5], options), legend=false,grid=false)
-    bar!(ans,alpha=1.5,xrotation=50,label="", xticks=(1:1:length(options),options),color="red",subplot=1)
-    plot!(title=question,titlefont=font(10,"Noto Sans Mono"))
-    plot!(ylabel="Número de respostas",titlefont=font(10,"Noto Sans Mono"))
-    y = collect(0.01:.01:1.0)
-    ticks = collect(0.0:0.1:1.0)
-#   yticks!([0:Statistics.mean(ans):maximum(ans);],) 
-    savefig("./plot.pdf")
+function barGraph(question, x , options, ans,fileName)
+   bar!(ans,alpha=1,xrotation=0,label="", xticks=(1:1:length(options),options),color="red",subplot=1)
+
+    plot!(title=question,titlefont=font(6,"Noto Sans Mono"))
+    plot!(ylabel="Número de respostas",titlefont=font(8,"Noto Sans Mono"))
+
+   # closeall()
+
+    figName = string("./graficos/", fileName, ".pdf");
+    savefig(figName);
 end
 
 
