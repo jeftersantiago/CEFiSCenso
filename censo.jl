8c04d7d632383cb30e592dcf54bae89597e9ad40
jefter66 <jeftersantiago@protonmail.com>
2021-10-18 14:47:51 -0300

    censo institucional

--- censo.jl
+++ censo.jl
@@ -5,6 +5,7 @@ using StatsPlots
 # deixar isso dinamico também, permitir passar o nome do arquivo em uma função
 global data=0;
 global option=0;
+global opt=0;
 # lista com as perguntas
 global question=0;
 
@@ -19,9 +20,11 @@ function setFile(path,isData, removeAt)
        global  question = getindex(names(option), 1:length(names(option)))
     end
 end
+
 # Percorre a lista de alternativa e remove os indices missing
 # Retorna a lista de opções, a partir da lista é possivel chamar a função
 # de gráfico e etc.
+# TODO: colocar o tipo de opt
 function alternatives(opt)
     i = 1;
     n = length(opt) -  length(findall(ismissing,opt));
@@ -38,6 +41,7 @@ function alternatives(opt)
     popfirst!(list);
     return reverse(list,1,length(list));
 end
+
 # Retorna uma lista com os valores (número de escolhas) para cada opção
 function setData(dataList,optList)
     list = Array{Int64,1}(undef,length(optList));
@@ -56,32 +60,125 @@ function setData(dataList,optList)
     return list;
 end
 
+
+function relateTables(l)
+#    if isequal(string(optList[i]), string(dataList[x]))
+#        list[i]+=1;
+#    if isequal(string(opt[i])), string(data[i,1])
+
+   k=1; # to run for all elements of the data
+   while k in 1:size(data,2)
+       i=1;# controls index of alternatives for each question
+       # When it leaves this loop all graphs for 1 of the choosen
+       # option will be ready to be transform into graph
+       global opt = alternatives(option[!,question[k]]);
+       list = Array{Int64,1}(undef,length(opt));
+
+       while i in 1:length(opt)
+           j=1; # run through all answers for the question in the data
+           while j in 1:size(data,1)
+#               if isequal(string(opt[i]),string(data[j,1]))
+                   # create a list in which will run to generate the graphs
+#                   list[i]+=1;
+#               end
+#               print("\n lista[i] = ", list[i]);
+               j+=1;
+#               print("\n", show(list));
+           end
+           i+=1;
+           print("\n List  =  ",length(list))
+       end
+       # generate the graphs here
+       # TODO: change function to accept a title
+       # function graph(title)
+       # graph(string(opt[i]))
+#       title= string(question[k])
+#       fileName = string("Gráfico - ",string(k));
+#       genGraph(title,list,opt,fileName);
+       k+=1;
+   end
+end
+
+
+# 1° - listar n° respostas pra cada disciplina.
+# 2° - da lista de opções de disciplinas rodar o loop
+#  por exemplo: disciplina 1: algebra linear
+#  roda loop pegando valor total de pessoas que responderam algelin
+#  roda loop pegando respostas da pergunta x pra quem respondeu algelin
+function relateTables()
+    n = 1;
+   global ans = data[!,n];
+   global opt = alternatives(option[!,question[n]]);
+#   global out = setData(ans,opt);
+#   fileName = string("Grafico - ",string(n));
+#   genGraph(question[n],out,opt,fileName)
+
+    list = Array{Int64,1}(undef,length(opt));
+    i=1;
+    while i in 1:length(opt)
+        list[i] = 0;
+        x=1;
+        
+        while x in 1:size(data,1)
+#            fileName="teoricas",i;
+#            figName = string("./Graficos/", fileName,".pdf");
+#            if isequal(string(opt[i]), string(data[x]))
+            # se entrar  aqui fazer uma lista com todas respostas seguintes
+            # para cada vez que entrar adicionar à lista
+
+            # Ex: entrou em intro a fiscomp -> cata todas respostas todas vezes que entrar aqui
+            # No final rodar o setData-> genGraph a partir desses valores novos
+
+#                list[i]+=1;
+#                print("list[i] = " ,list[i]);
+#            end
+            x+=1;
+        end
+        i+=1;
+    end
+end
+
+# Gera o gráfico - em barras por enquanto
+# question = titulo do gráfico
+# answers = dados, lista de valores de cada respostas
+# options = as opções de respostas, isso fica como legenda e tals
+# fileName = nome que o arquivo .pdf vai ter
 function genGraph(question,answers,options,fileName)
-    bar(answers,alpha=1,xrotation=0,label="",xticks=(1:1:length(options),options), color="red", subplot=1)
-    plot!(title=question,titlefont=font(6,"Roboto Regular"))
-    plot!(ylabel="Número de respostas",titlefont=font(8,"Roboto Regular"))
+
+    palette = cgrad(:Set3_12);#  cgrad(:BrBG_10);
+    cs=rand(1:10, 10);
+    
+   
+    #bar(answers,alpha=1,xrotation=0,label="",xticks=(1:1:length(options),options), color=color=[palette[i] for i in cs], legend=:topleft, subplot=1)
+    bar((1:length(options))', answers', color= [palette[i] for i in cs]')#, label=[string(options[i]) for i in ])
+
+    plot!(ylabel="Respostas",titlefont=font(8,"Roboto Regular"))
+
     closeall()
-    figName = string("./graficos/", fileName,".pdf");
+    figName = string("./Graficos/", fileName,".pdf");
     savefig(figName);
 end
 
+
+# TODO: colocar o tipo de n
 function runForIndex(n)
    global opt = alternatives(option[!,question[n]]);
    global ans = data[!,n];
    global out = setData(ans,opt);
-   fileName = string("pergunta",string(n));
+   fileName = string("Grafico - ",string(n));
    genGraph(question[n],out,opt,fileName)
 end
 
 function runForAll(dataFile,optionsFile, removeColumnsUpTo)
     setFile(dataFile, true, removeColumnsUpTo);
     setFile(optionsFile,false,removeColumnsUpTo);
+    #just for now
+    #global opt = alternatives(option[!,question[n]]);
+    #relateTables()
     i=1;
-    while i in 1:size
-        println("File nº ", i);
+    while i in 1:length(names(data))
+        println("Gerando arquivo nº ", i);
         runForIndex(i);
         i+=1;
     end
-    println("======== Acabou ========== \n");
 end
-
