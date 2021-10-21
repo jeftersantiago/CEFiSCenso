6f3bf821b4ebac766c53b45bea8119ee1e5cc637
jefter66 <jeftersantiago@protonmail.com>
2021-10-21 16:24:33 -0300

    orgnizing

--- src/genCharts.jl
+++ src/genCharts.jl
@@ -1,7 +1,6 @@
 using Plots,StatsPlots
 using StatsBase
 
-
 # qt = titulo do gráfico
 # ans = dados, lista de valores de cada respostas
 # opt = as opções de respostas, isso fica como legenda e tals
@@ -13,15 +12,15 @@ function barChart(qt,ans,opt,fileName)
     # see https://docs.juliaplots.org/latest/generated/colorschemes/
     palette = cgrad(:Set3_12);
     cs=rand(1:10, 10);
+    colours = [palette[i] for i in cs];
 
     legend = opt[:,:];
     legend = reshape(legend,(1,length(legend)));
     
-    bar((1:length(opt))', ans', title=qt,color= [palette[i] for i in cs]', label=legend);
+    bar((1:length(opt))', ans', title=qt,color= colours', label=legend);
     
     plot!(ylabel="n° de respostas",titlefont=font(8,"Roboto Regular"));
 
     closeall();  
     savefig(fileName);
 end
-
